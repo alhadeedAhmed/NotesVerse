8476291ebf3bbba3485c2303f8298bbc07f3768b
alhadeedahmed <hadeedsudheer22@gmail.com>
2025-07-18 13:04:04 +0500

    Made changes for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -5,14 +5,21 @@ import cors from "cors";
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 8000;
-
+const __dirname = path.resolve();
 // built-in middleware > it allows us to get access to req.body we used in create update, otherwise it give undefined, so we use it, it get access to title and content here
-app.use(cors());
+if (process.env.NODE_ENV !== "production") {
+  app.use(
+    cors({
+      origin: "http://localhost:5173",
+    })
+  );
+}
 app.use(express.json());
 app.use(rateLimiter);
 
@@ -23,6 +30,14 @@ app.use((req, res, next) => {
 }); // output: Request method is GET and Request URL is /api/notes
 
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 connectDB().then(() => {
   app.listen(PORT, () => {

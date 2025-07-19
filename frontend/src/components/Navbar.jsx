fa0efd4d7324fd3fdafe856ac9d9f43450101619
alhadeedahmed <hadeedsudheer22@gmail.com>
2025-07-19 10:08:58 +0500

    Changed size of NoteVerse logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header className="bg-base-300 border-b border-base-content/10">
       <div className="mx-auto max-w-6xl p-4">
         <div className="flex items-center justify-between">
-          <h1 className="text-3xl font-bold text-primary font-mono tracking-tight">
+          <h1 className="text-2xl font-bold text-primary font-mono tracking-tight">
             NotesVerse
           </h1>
           <div className="flex items-center gap-4">
